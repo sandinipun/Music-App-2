ab9f39120a13baf2d50c836445dfa0ec54142135
sandinipun <74046128+sandinipun@users.noreply.github.com>
2020-11-18 16:51:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song1 = "";
+song2 = "";
+function preload()
+{
+	song1 = loadSound("Alan Walker & Ava Max - Alone, Pt. II.mp3");
+	song2 = loadSound("Marshmello - Alone (Official Music Video).mp3");
+}
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+}
+function draw() {
+	image(video, 0, 0, 600, 500);
+}
